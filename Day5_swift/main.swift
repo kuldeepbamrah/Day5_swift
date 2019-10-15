1f994a4182ddfd71ebc24c37d403ab79610df74e
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-10-15 17:36:26 -0400

    initial commit

---
+++ Day5_swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day5_swift
+//
+//  Created by MacStudent on 2019-10-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
